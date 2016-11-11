f2949c211ab44ba31b3cc250ef36969cdb67cf6b
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-11-12 02:23:24 +0800

    Add stats route

--- GeeCS_9_Hackathon/stats/views.py
+++ GeeCS_9_Hackathon/stats/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'stats/index.html', context)
+
+def stats(request):
+    context = {}
+    return render(request, 'stats/stats.html', context)
