bdea9b67c78f5a031ae74a3dfc3040c267778e76
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-11-12 04:14:36 +0800

    Add subject model

--- GeeCS_9_Hackathon/stats/models.py
+++ GeeCS_9_Hackathon/stats/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class  Subject(models.Model):
+    slots = models.IntegerField(default=0)
+    demand = models.IntegerField(default=0)
+    subject = models.CharField(max_length=50, default="subj")
+    section = models.CharField(max_length=20, default="section")
+    prof = models.CharField(max_length=100,  default="prof")
+    sched = models.CharField(max_length=1000, default="sched")
+
+    def __str__(self):
+        return '{} {} {}/{} {} {}'.format(self.subject, self.section, self.demand, self.slots, self.prof, self.sched)
