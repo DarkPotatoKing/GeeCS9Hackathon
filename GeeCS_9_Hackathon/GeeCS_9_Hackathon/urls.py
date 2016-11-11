4dafdb2059e6d21452d22b47997f5437c06e35ec
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-11-12 01:48:06 +0800

    Change route from /index to /hack/index

--- GeeCS_9_Hackathon/GeeCS_9_Hackathon/urls.py
+++ GeeCS_9_Hackathon/GeeCS_9_Hackathon/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^index/', include('hack.urls')),
+    url(r'^hack/', include('hack.urls')),
     url(r'^admin/', admin.site.urls),
 ]
