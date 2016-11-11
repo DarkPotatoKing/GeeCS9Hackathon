721bbeb65c7c7050b5e572d86273e3bba72ac4de
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-11-12 02:26:16 +0800

    Add routes for demand and recommend

--- GeeCS_9_Hackathon/stats/urls.py
+++ GeeCS_9_Hackathon/stats/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^stats$', views.stats, name='stats')
+    url(r'^stats$', views.stats, name='stats'),
+    url(r'^recommend$', views.recommend, name='recommend'),
+    url(r'^demand$', views.demand, name='demand'),
 ]
--- GeeCS_9_Hackathon/stats/views.py
+++ GeeCS_9_Hackathon/stats/views.py
@@ -7,3 +7,12 @@ def index(request):
 def stats(request):
     context = {}
     return render(request, 'stats/stats.html', context)
+
+def recommend(request):
+    context = {}
+    return render(request, 'stats/recommend.html', context)
+
+def demand(request):
+    context = {}
+    return render(request, 'stats/demand.html', context)
+
