a3f31184caf40358d325e2d0cf5e0dcbb8d5bf0a
DarkPotatoKing <kyle.rosales97@gmail.com>
2016-11-12 11:08:31 +0800

    Add single prob

--- GeeCS_9_Hackathon/stats/views.py
+++ GeeCS_9_Hackathon/stats/views.py
@@ -2,13 +2,34 @@ from django.shortcuts import render
 
 from .models import Subject
 
+class SubjectStats(object):
+
+    def __init__(self, rank, slots, demand, subject, section, units, prof, sched):
+        self.rank = rank
+        self.slots = slots
+        self.demand = demand
+        self.subject = subject
+        self.section = section
+        self.units = units
+        self.prof = prof
+        self.sched = sched
+        self.total_prob = 0
+
+
 def index(request):
     context = {}
     return render(request, 'stats/index.html', context)
 
 def stats(request):
-    subjects = filter(lambda x: x.rank > 0, Subject.objects.all())
+    s = filter(lambda x: x.rank > 0, Subject.objects.all())
+    subjects = [SubjectStats(rank=i.rank, slots=i.slots, demand=i.demand, subject=i.subject, section=i.section, units=i.units, prof=i.prof, sched=i.sched) for i in s]
+    for i, val in enumerate(subjects):
+        subjects[i].total_prob = int(float(val.slots) / float(val.demand) *10000) / 100.0
+
+        if subjects[i].total_prob  >= 100:
+            subjects[i].total_prob = 100.0
     context = { 'subjects': subjects }
+
     return render(request, 'stats/stats.html', context)
 
 def recommend(request):
